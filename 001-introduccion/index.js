4d5ab352d74e20a1f9ea685c044528e3286dc1e0
davidlealo <davidlealo@gmail.com>
2024-09-17 10:11:05 -0300

    Explicación GlobalThis

--- 001-introduccion/index.js
+++ 001-introduccion/index.js
@@ -1,3 +1,13 @@
 console.log('Hola Mundo!')
 console.info('Hola info')
 console.error('Hola error')
+
+/* 
+Node JS no tiene el Windows como sería al ejecutar JS desde el navegador.
+El equivalente en Node es Global.
+Pero tenemos es un objeto que se llama GlobalThis que sirve
+para navegador y desktop.
+Desde GlobalThis sale todo, incluso console.log es GlobalThis.console.log()
+*/
+
+console.log(globalThis)
