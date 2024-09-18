3ba8e95c8f1e660962241d9afdeb06abad693187
davidlealo <davidlealo@gmail.com>
2024-09-18 20:32:37 -0300

    process

---
+++ 001-introduccion/004modulos-nativos/app/process.js
@@ -0,0 +1,16 @@
+// Argumentos de entrada
+//console.log(process.argv)
+
+//Controlar el proceso y su salida
+//process.exit(1)
+
+//Controlar eventos del proceso
+//process.on('exit', ()=>{
+    //limpiar los recursos
+//})
+
+//Current working directory
+console.log(process.cwd())
+
+//Plataforma
+console.log(process.env.OPENAI_API_KEY)
