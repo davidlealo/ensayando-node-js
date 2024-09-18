8162d3f4a70a3ab1cf01ef224f449dc46c251652
davidlealo <davidlealo@gmail.com>
2024-09-18 20:23:53 -0300

    app ls para ver los archivos como ls de linux

---
+++ 001-introduccion/004modulos-nativos/app/ls.js
@@ -0,0 +1,13 @@
+// Comando ls para ver las carpetas
+
+const fs = require('node:fs')
+
+fs.readdir('.', (err, files)=>{
+    if (err){
+        console.error('Error al leer el directorio', err)
+        return;
+    }
+    files.forEach(file=>{
+        console.log(file)
+    })
+})
