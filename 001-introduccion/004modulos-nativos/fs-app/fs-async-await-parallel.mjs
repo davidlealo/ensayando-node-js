8316b5cf25d3341f68dc9558b162a26ca3b9dab0
davidlealo <davidlealo@gmail.com>
2024-09-18 09:24:15 -0300

    async parallel ES

--- 001-introduccion/004modulos-nativos/fs-app/fs-async-await-parallel.mjs
+++ 001-introduccion/004modulos-nativos/fs-app/fs-async-await-parallel.mjs
@@ -7,24 +7,3 @@ Promise.all([
     console.log(first)
     console.log(second)
 })
-
-/*
-
-//IIFE - Inmediatly Invoked Function Expression
-//(()=>{})()
-
-(
-    async () => {
-        console.log('leyendo el primer archivo...')
-        const txt = await fs.readFile('./hola.txt', 'utf-8',).then(txt => {
-            console.log(txt)
-        })
-
-        console.log('Puedo hacer cosas por mientras')
-
-        console.log('leyendo el segundo archivo...')
-        const text = await fs.readFile('./lista.txt', 'utf-8').then(text => {
-            console.log(text)
-        })
-    })()
-*/
