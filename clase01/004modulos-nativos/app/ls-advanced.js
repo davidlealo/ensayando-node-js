bdb1d1f8150cfa944aa5ae4522f7f1b1e38b3e30
davidlealo <davidlealo@gmail.com>
2024-09-18 21:54:46 -0300

    mejoras con linter

--- clase01/004modulos-nativos/app/ls-advanced.js
+++ clase01/004modulos-nativos/app/ls-advanced.js
@@ -2,13 +2,13 @@ const fs = require('node:fs/promises')
 
 const folder = process.argv[2] ?? '.'
 
-fs.readdir(folder).then(files=>{
-    files.forEach(file=>{
-        console.log(file)
-    })
-}).catch(err =>{
-    if (err){
-        console.log('Error al leer el directorio:', err)
-        return;
-    }
+fs.readdir(folder).then(files => {
+  files.forEach(file => {
+    console.log(file)
+  })
+}).catch(err => {
+  if (err) {
+    console.log('Error al leer el directorio:', err)
+    return;
+  }
 })
