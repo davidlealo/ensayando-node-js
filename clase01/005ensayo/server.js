e569eddb2b68398875e3e7cd3a3eeb554d0eabe6
davidlealo <davidlealo@gmail.com>
2024-09-19 13:35:35 -0300

    server listen

--- clase01/005ensayo/server.js
+++ clase01/005ensayo/server.js
@@ -4,3 +4,7 @@ const server = http.createServer((req, res) => {
     console.log('Servidor iniciado')
     res.end('Hola!')
 })
+
+server.listen(0, () => {
+    console.log(`server listening on port http://localhost:${server.address().port}`)
+  })
