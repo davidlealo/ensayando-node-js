2c37383a15f10f68edbe45cbbc0b012b362798aa
davidlealo <davidlealo@gmail.com>
2024-09-20 17:56:39 -0300

    servidor con header

--- clase02/1.http.js
+++ clase02/1.http.js
@@ -2,11 +2,21 @@ const http = require('node:http')
 
 const desiredPort = process.env.PORT ?? 1234
 
-const server = http.createServer((req, res) => {
-    console.log('request received', req.url)
-    res.end('Hola mundo')
-    
-})
+const processRequest = (req, res) => {
+    res.setHeader('content-type', 'text/html, charset=utf-8')
+
+    if (req.url == '/') {
+        res.statusCode = 200
+        res.end('<h1>Bienvenido al inicio de mi sitio web</h1>')}
+    else if (req.url == '/contacto') {
+        res.statusCode = 200
+        res.end('<h1>Contacto</h1>')
+    } else {
+        res.statusCode = 404
+        res.end('<h1>404</h1>')
+    }}
+
+const server = http.createServer(processRequest)
 
 server.listen(desiredPort, () =>{
     console.log(`server listening on port http://localhost:${desiredPort}`)
