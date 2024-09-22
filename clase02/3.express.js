22969569942b889676bb345678034ef2248b810b
davidlealo <davidlealo@gmail.com>
2024-09-22 19:40:12 -0300

    ruta post del ejercicio anterior

--- clase02/3.express.js
+++ clase02/3.express.js
@@ -5,8 +5,22 @@ const app = express()
 const PORT = process.env.PORT ?? 1234
 
 app.get('/', (req, res) => {
-  console.log('Hola mundo')
-  res.status(200).send('<h1>Mi página</j1>')
+  console.log('Cargaste correctamente la  ruta "/" de tu sitio')
+  res.send('<h1>Mi página</j1>')
+})
+
+app.post('/pokemon', (req, res) => {
+  let body = ''
+
+  req.on('data', chunk => {
+    body += chunk.toString()
+  })
+
+  req.on('end', () => {
+    const data = JSON.parse(body)
+    res.writeHead(201, { 'content-type': 'application/json; charset=utf=8' })
+    data.rimestamp = Date.now()
+  })
 })
 
 app.listen(PORT, () => {
