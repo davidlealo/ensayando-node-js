4bc15c6a1438df064caddf890023343d4002fcf3
davidlealo <davidlealo@gmail.com>
2024-09-22 19:20:48 -0300

    primera parte obteniendo el '/'

--- clase02/3.express.js
+++ clase02/3.express.js
@@ -4,3 +4,11 @@ const app = express()
 
 const PORT = process.env.PORT ?? 1234
 
+app.get('/', (req, res) => {
+  console.log('Hola mundo')
+  res.status(200).send('<h1>Mi página</j1>')
+})
+
+app.listen(PORT, () => {
+  console.log(`server listening on port http://localhost:${PORT}`)
+})
