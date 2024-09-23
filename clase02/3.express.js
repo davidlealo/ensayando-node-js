7d9d88a1833e2193cb451c4205700e82ec90332a
davidlealo <davidlealo@gmail.com>
2024-09-22 21:08:26 -0300

    nuevo organizacion usando middlerware

--- clase02/3.express.js
+++ clase02/3.express.js
@@ -5,27 +5,38 @@ app.disable('x-powered-by')
 
 const PORT = process.env.PORT ?? 1234
 
-app.get('/pokemon/ditto', (req, res) => {
-  console.log('Cargaste correctamente la  ruta "/pokemon/ditto" de tu sitio')
-  res.json(ditto)
-})
+app.use(express.json())
+
+// app.use((req, res, next) => {
+//   if (req.method !== 'POST') return next()
+//   if (req.headers['content-type'] !== 'application/json') return next()
+
+//   // solo llegan request que son POST y que tienen el header Content-Type: application/json
+//   let body = ''
+
+//   // escuchar el evento data
+//   req.on('data', chunk => {
+//     body += chunk.toString()
+//   })
+
+//   req.on('end', () => {
+//     const data = JSON.parse(body)
+//     data.timestamp = Date.now()
+//     // mutar la request y meter la información en el req.body
+//     req.body = data
+//     next()
+//   })
+// })
 
 app.post('/pokemon', (req, res) => {
-  let body = ''
-
-  req.on('data', chunk => {
-    body += chunk.toString()
-  })
-
-  req.on('end', () => {
-    const data = JSON.parse(body)
-    data.timestamp = Date.now()
-    res.status(201).json(data)
-  })
+// req.body deberíamos guardar en bbdd
+  res.status(201).json(req.body)
+  console.log('Cargaste correctamente la  ruta "/pokemon/ditto" de tu sitio')
 })
 
 app.use((req, res) => {
   res.status(404).end('<h1>Error 404</h1>')
+  console.log('Error 404')
 })
 
 app.listen(PORT, () => {
