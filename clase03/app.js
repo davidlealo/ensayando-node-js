6ef67800826bf8f2de03a746294bb108dd3c6277
davidlealo <davidlealo@gmail.com>
2024-09-25 17:56:48 -0300

    cors

--- clase03/app.js
+++ clase03/app.js
@@ -1,7 +1,29 @@
 const express = require('express')
 const { validateMovie, validatePartialMovie } = require('./schemas/movies')
 app = express()
+const cors = require('cors')
+
 app.use(express.json())
+app.use(cors({
+    origin: (origin, callback) => {
+      const ACCEPTED_ORIGINS = [
+        'http://localhost:8080',
+        'http://localhost:1234',
+        'https://movies.com',
+        'https://davidleal.cl'
+      ]
+  
+      if (ACCEPTED_ORIGINS.includes(origin)) {
+        return callback(null, true)
+      }
+  
+      if (!origin) {
+        return callback(null, true)
+      }
+  
+      return callback(new Error('Not allowed by CORS'))
+    }
+  }))
 const movies = require('./movies.json')
 const z = require('zod')
 app.disable('x-powered-by')
@@ -53,6 +75,19 @@ app.post('/movies', (req, res) => {
     res.status(201).json(newMovie) // actualiza la caché del cliente
 
 })
+
+app.delete('/movies/:id', (req, res) => {
+    const { id } = req.params
+    const movieIndex = movies.findIndex(movie => movie.id === id)
+  
+    if (movieIndex === -1) {
+      return res.status(404).json({ message: 'Movie not found' })
+    }
+  
+    movies.splice(movieIndex, 1)
+  
+    return res.json({ message: 'Movie deleted' })
+  })
 
 // Actualizar una película 
 app.patch('/movies/:id', (req, res) => {
