7eafbd476081d1e14b2bc53257c8c802f194fe7c
davidlealo <davidlealo@gmail.com>
2024-09-24 23:33:56 -0300

    ruta para una pelñicula

--- clase03/app.js
+++ clase03/app.js
@@ -7,8 +7,18 @@ app.get('/', (req, res) => {
     res.json({message: 'Hola mundo'})
 })
 
+// Todas las películas
 app.get('/movies', (req, res) =>{
     res.json(movies)
+})
+
+// Películas por id
+app.get('/movies/:id', (req, res) =>{
+    const { id } = req.params
+    const movie = movies.find( movie => movie.id === id)
+    if (movie) return res.json(movie)
+
+    res.status(404).json({message: 'Movie not found'})
 })
 
 const PORT = process.env.PORT ?? 1234
