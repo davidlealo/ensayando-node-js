d829791797a3d263dff255e919c34593dbcc4e30
davidlealo <davidlealo@gmail.com>
2024-10-20 16:46:57 -0300

    bd en gitignore

--- clase07/index.js
+++ clase07/index.js
@@ -1,16 +1,16 @@
 import express from 'express'
-import {PORT} from './config.js'
+import { PORT } from './config.js'
 import { userRepository } from './user-repository.js'
 
 const app = express()
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send('Estas en /')
+  res.send('Estas en /')
 })
 
 app.post('/login', (req, res) => {
-  
+
 })
 
 app.post('/register', (req, res) => {
@@ -20,19 +20,19 @@ app.post('/register', (req, res) => {
   try {
     const id = userRepository.create({ username, password })
     res.send(id)
+  } catch (error) {
+    res.status(400).send(error.message)
   }
-  catch (error) {
-    res.status(400).send(error.message)}
 })
 
 app.post('/logout', (req, res) => {
-  
+
 })
 
 app.get('/protected', (req, res) => {
-  
+
 })
 
-app.listen(PORT, () =>{
-    console.log(`Server running in port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server running in port ${PORT}`)
 })
